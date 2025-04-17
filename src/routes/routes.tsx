0aef9ad8e01047941288ded49508bfcda02c1425
faunly <faunlyanderson@gmail.com>
2025-04-17 21:58:24 +0300

    refactor: improved routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,21 +1,17 @@
-import { Route, Routes } from 'react-router'
+import { RouteObject, useRoutes } from 'react-router-dom'
 import Profile from '../pages/Profile'
 import TodoList from '../pages/TodoList'
 import { Navigate } from 'react-router'
 
+const routesConfig: RouteObject[] = [
+    { path: '/', element: <Navigate to="/todo" replace /> },
+    { path: '/todo', element: <TodoList /> },
+    { path: '/profile', element: <Profile /> },
+]
+
 const AppRoutes = () => {
-    const navigationRoutes = [
-        { path: '/', element: <Navigate to="/todo" replace /> },
-        { path: '/todo', element: <TodoList></TodoList> },
-        { path: '/profile', element: <Profile></Profile> },
-    ]
-    return (
-        <Routes>
-            {navigationRoutes.map(route => (
-                <Route key={route.path} path={route.path} element={route.element} />
-            ))}
-        </Routes>
-    )
+    const routes = useRoutes(routesConfig)
+    return routes
 }
 
 export default AppRoutes
