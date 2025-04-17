74b8c21151247a4e253c8feeb518e913c6e174ce
faunly <faunlyanderson@gmail.com>
2025-04-17 21:31:54 +0300

    add: replaced setAndAlert to Notification from AntD

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,70 +1,76 @@
-import { Flex, Tabs, Typography } from 'antd'
-import TaskList from '../components/TaskList/TaskList'
-import { filterTypes } from '../types/filter'
 import { useCallback, useEffect, useState } from 'react'
-import { CategoriesType, TasksType } from '../types/todolist'
-import { Content, Header } from 'antd/es/layout/layout'
-import AddTask from '../components/AddTask/AddTask'
 import { changeDataTask, deleteTask, getTasksByCategory } from '../api/http'
+import { Flex, Tabs, message, notification } from 'antd'
+import { Content, Header } from 'antd/es/layout/layout'
+import { CategoriesType, TasksType } from '../types/todolist'
+import { filterTypes } from '../types/filter'
+import AddTask from '../components/AddTask/AddTask'
+import TaskList from '../components/TaskList/TaskList'
 
 const TodoList = () => {
     const [tasks, setTasks] = useState<TasksType[]>([])
     const [filter, setFilter] = useState<filterTypes>('all')
     const [infoTasks, setInfoTasks] = useState<CategoriesType>()
-    const [isFetching, setIsFetching] = useState(false)
+    const [messageApi, messageContextHolder] = message.useMessage()
+    const [notificationApi, notificationContextHolder] = notification.useNotification()
+
+    const showAlert = (error: string) => {
+        notificationApi.error({
+            message: `${error}`,
+            placement: 'top',
+            showProgress: true,
+            pauseOnHover: false,
+        })
+    }
 
     const handleDeleteTask = async (id: number) => {
         try {
-            setIsFetching(true)
             await deleteTask(id)
             await fetchTasksByFilter()
         } catch {
-            setAndAlertError('Ошибка удаления задачи!')
-        } finally {
-            setIsFetching(false)
+            showAlert('Ошибка удаления задачи!')
         }
     }
 
     const handleChangeDataTask = async (id: number, titleTask: string, isDone: boolean) => {
         try {
-            setIsFetching(true)
             await changeDataTask(id, titleTask, isDone)
             await fetchTasksByFilter()
         } catch {
-            setAndAlertError('Ошибка изменения задачи!')
-        } finally {
-            setIsFetching(false)
+            showAlert('Ошибка изменения задачи!')
         }
     }
 
     const fetchTasksByFilter = useCallback(
         async (fetchFilter = filter) => {
             try {
-                setIsFetching(true)
                 const todos = await getTasksByCategory(fetchFilter)
                 setTasks(todos.data)
                 setInfoTasks(todos.info)
             } catch {
-                setAndAlertError('Ошибка получения задач!')
-            } finally {
-                setIsFetching(false)
+                showAlert('Ошибка получения задач!')
             }
         },
         [filter],
     )
 
     useEffect(() => {
+        messageApi.open({
+            type: 'loading',
+            content: 'Получение задач...',
+            duration: 1,
+        })
+    }, [])
+
+    useEffect(() => {
         fetchTasksByFilter()
+
         const refetch = setInterval(() => {
             fetchTasksByFilter()
         }, 5000)
 
         return () => clearInterval(refetch)
     }, [filter])
-
-    const setAndAlertError = (error: string) => {
-        alert(error)
-    }
 
     const itemsTabs: { key: filterTypes; label: string }[] = [
         {
@@ -104,7 +110,8 @@ const TodoList = () => {
                         handleDeleteTask={handleDeleteTask}
                         handleChangeDataTask={handleChangeDataTask}
                     />
-                    {isFetching && <Typography.Title level={5}>Fetching tasks...</Typography.Title>}
+                    {messageContextHolder}
+                    {notificationContextHolder}
                 </Flex>
             </Content>
         </Flex>
