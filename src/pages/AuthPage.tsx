cd027122075b17f0f798f99957e0a510eb792120
Faunly <faunlyanderson@gmail.com>
2025-08-02 09:58:15 +0300

    refactor and add: renamed LoginPage to AuthPage, add rules for form items

---
+++ src/pages/AuthPage.tsx
@@ -0,0 +1,19 @@
+import { Flex, Image } from 'antd';
+
+import backgroundImage from '../assets/AuthPage/background.png';
+import RegisterForm from '../components/RegisterForm/RegisterForm';
+import LoginForm from '../components/LoginForm/LoginForm.tsx';
+import { useLocation } from 'react-router';
+
+const AuthPage = () => {
+    const location = useLocation();
+
+    return (
+        <Flex style={{ backgroundColor: '#FFFFFF', width: '100%' }}>
+            <Image src={backgroundImage} preview={false} height={'100vh'} />
+            {location.pathname === '/login' ? <LoginForm /> : <RegisterForm />}
+        </Flex>
+    );
+};
+
+export default AuthPage;
